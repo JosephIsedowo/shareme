57ff2c091212efd7ffd5a2e2a98761da59f6e0bc
Joe-tec <61061657+Joe-tec@users.noreply.github.com>
2023-02-21 13:09:11 -0500

    work

---
+++ schemas/index.ts
@@ -0,0 +1,20 @@
+
+import user from './user'
+
+import pin from './pin'
+
+import comment from './comment'
+
+import postedBy from './postedBy'
+
+import save from './save'
+
+
+
+export const schemaTypes = [
+  // Document types
+  user,pin,comment,postedBy,save
+
+  // Other types
+
+]
---
+++ schemas/user.ts
@@ -0,0 +1,21 @@
+import {defineField, defineType} from 'sanity'
+import {MdPerson as icon} from 'react-icons/md'
+
+export default defineType({
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  icon,
+  fields: [
+    defineField({
+      name: 'userName',
+      title: 'UserName',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    }),
+  ],
+})
